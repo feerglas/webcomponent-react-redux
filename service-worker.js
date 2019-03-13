38278f511958490616ab19b888f144d8677eec42
Yves Tscherry <yves.tscherry@unic.com>
2019-03-13 22:03:33 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/webcomponent-react-redux/precache-manifest.2cc9b17f55e6acc491fc6a94d011d511.js"
+  "/webcomponent-react-redux/precache-manifest.a0bfa9f0dbca3f50027e69c57df6587b.js"
 );
 
 workbox.clientsClaim();
